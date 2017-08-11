688c0e31733bc23f79a2e9798213ffcf0d03282a
Sasikala Mani <mani.37@ruan.cse.ohio-state.edu>
2017-08-11 15:08:42 -0400

    proper MRR

---
+++ postold.py
@@ -0,0 +1,45 @@
+import prepold
+import MLP
+
+(testDict) = prepold.dict()
+
+sortedDict = dict()
+for key in testDict:
+    x = testDict[key]
+    index = [0]*len(x)
+    for i in range(len(x)):
+        index[x.index(max(x))] = i
+        x[x.index(max(x))] = min(x)-1
+    sortedDict[key] = index
+
+testQuestions = prepold.ques()
+
+(output) = MLP.test_mlp()
+
+outputDict = dict()
+for i in range(len(output)):
+    ques = testQuestions[i]
+    if(ques not in outputDict):
+        outputDict[ques] = [output[i]]
+    else: outputDict[ques] += [output[i]]
+
+
+outputSorted = dict()
+for key in outputDict:
+    x = testDict[key]
+    index = [0]*len(x)
+    for i in range(len(x)):
+        index[x.index(max(x))] = i
+        x[x.index(max(x))] = min(x)-1
+    outputSorted[key] = index
+
+
+final = 0
+for ques in outputSorted:
+    x = outputSorted[ques]
+    num = x.index(0)
+    y = sortedDict[ques]
+    num2 = y[num]
+    prob = (num2) + 1
+    final += (1/float(prob))
+print(final/(float(580)))
